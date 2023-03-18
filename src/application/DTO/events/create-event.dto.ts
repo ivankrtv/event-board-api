0cd9a97921bc6143911acd13a2ec24d85cc9861c
ivankrtv <mega.krotov@mail.ru>
2023-03-18 17:11:54 +0300

    fix: create new event with categories #18

--- src/application/DTO/events/create-event.dto.ts
+++ src/application/DTO/events/create-event.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { EventsGenderEnum } from '../../../enums/events-gender.enum';
+import { EventCategory } from '../../../enums/event-category';
+import { EventMood } from '../../../enums/event-mood';
 
 export class CreateEventDto {
   @ApiProperty({ example: 'Мафия на 12 человек' })
@@ -13,6 +15,12 @@ export class CreateEventDto {
 
   @ApiProperty()
   peopleNeed: number;
+
+  @ApiProperty({ enum: EventCategory })
+  category: EventCategory;
+
+  @ApiProperty({ enum: EventMood })
+  mood: EventMood;
 
   @ApiProperty({ example: '2023-11-20T18:00:00' })
   startAt: Date;
