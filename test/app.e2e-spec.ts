b242132708820cf35b6851ca222f1949e216b4d6
ivankrtv <mega.krotov@mail.ru>
2023-05-01 23:24:00 +0300

    add: clear database universal command

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 
 import { AppModule } from './../src/app.module';
+import {UserTestBuilder} from "../src/infrastructure/entity-builders/user.test-builder";
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,7 +17,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
-  });
+
+  afterAll(() => {
+    app.close();
+  })
 });
